414484bd0aefa6e45c460f211e04570dcb2ed6a1
Dmitry Lavnikevich <1469488+githaff@users.noreply.github.com>
2019-08-16 20:25:16 +0300

    Implement key debouncing

--- panda-workout.ino
+++ panda-workout.ino
@@ -1,28 +1,7 @@
 /*
-  Arduino Programs Blink
-
-  This sketch demonstrates the Keyboard library.
-
-  For Leonardo and Due boards only.
-
-  When you connect pin 2 to ground, it creates a new window with a key
-  combination (CTRL-N), then types in the Blink sketch, then auto-formats the
-  text using another key combination (CTRL-T), then uploads the sketch to the
-  currently selected Arduino using a final key combination (CTRL-U).
-
-  Circuit:
-  - Arduino Leonardo, Micro, Due, LilyPad USB, or Yún
-  - wire to connect D2 to ground
-
-  created 5 Mar 2012
-  modified 29 Mar 2012
-  by Tom Igoe
-  modified 3 May 2014
-  by Scott Fitzgerald
-
-  This example is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/KeyboardReprogram
+ * Panda Workout fitness machine remote control
+ *
+ * Author: Haff
 */
 
 #include "Keyboard.h"
@@ -44,22 +23,24 @@
 #define K_PG_UP  10
 #define K_PWR    11
 
-int pinStates[KEY_COUNT];
-
+int keyStates[KEY_COUNT] = { 0 };
+int keyStableStates[KEY_COUNT] = { 0 };
+unsigned long lastDebounces[KEY_COUNT] = { 0 };
+unsigned long debounceDelay = 10;
 
 void setup()
 {
   for (int i = 0; i < KEY_COUNT; i++) {
     pinMode(i, INPUT_PULLUP);
-    pinStates[i] = digitalRead(i);
+    keyStates[i] = HIGH;
   }
 
   Keyboard.begin();
 }
 
-void pressKey(int key)
+void pressKey(int pin)
 {
-  switch (key) {
+  switch (pin) {
     case K_UP     : Keyboard.press(KEY_UP);               break;
     case K_DOWN   : Keyboard.press(KEY_DOWN);             break;
     case K_LEFT   : Keyboard.press(KEY_LEFT);             break;
@@ -73,11 +54,12 @@ void pressKey(int key)
     case K_VOL_UP : Keyboard.press(KEY_MEDIA_VOLUME_INC); break;
     case K_PWR    : Keyboard.press(KEY_SYSTEM_WAKE_UP);   break;
   }
+  keyStableStates[pin] = LOW;
 }
 
-void releaseKey(int key)
+void releaseKey(int pin)
 {
-  switch (key) {
+  switch (pin) {
     case K_UP     : Keyboard.release(KEY_UP);               break;
     case K_DOWN   : Keyboard.release(KEY_DOWN);             break;
     case K_LEFT   : Keyboard.release(KEY_LEFT);             break;
@@ -91,19 +73,30 @@ void releaseKey(int key)
     case K_VOL_UP : Keyboard.release(KEY_MEDIA_VOLUME_INC); break;
     case K_PWR    : Keyboard.release(KEY_SYSTEM_WAKE_UP);   break;
   }
+  keyStableStates[pin] = HIGH;
+}
+
+void processKey(int pin)
+{
+  int state = digitalRead(pin);
+
+  if (state != keyStates[pin])
+    lastDebounces[pin] = millis();
+  keyStates[pin] = state;
+
+  if ((millis() - lastDebounces[pin]) <= debounceDelay)
+    return;
+  if (state == keyStableStates[pin])
+    return;
+
+  switch (state) {
+    case LOW:  pressKey(pin);   break;
+    case HIGH: releaseKey(pin); break;
+  }
 }
 
 void loop()
 {
-  for (int i = 0; i < KEY_COUNT; i++) {
-    int state = digitalRead(i);
-    if (state == LOW && pinStates[i] == HIGH) {
-      pressKey(i);
-    } else if (state == HIGH && pinStates[i] == LOW) {
-      releaseKey(i);
-    }
-    pinStates[i] = state;
-  }
-  // TODO: add bounce support
-  delay(50);
+  for (int i = 0; i < KEY_COUNT; i++)
+    processKey(i);
 }
